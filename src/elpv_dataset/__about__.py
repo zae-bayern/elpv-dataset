c216b448921e86cf380232684bf0f09554db5693
Sergiu Deitsch <sergiu.deitsch@gmail.com>
2024-10-13 21:38:27 +0200

    specify code license

--- src/elpv_dataset/__about__.py
+++ src/elpv_dataset/__about__.py
@@ -1,1 +1,4 @@
+# SPDX-FileCopyrightText: 2024 Sergiu Deitsch <sergiu.deitsch@gmail.com>
+#
+# SPDX-License-Identifier: Apache-2.0
 __version__ = '1.0.0.post1'
---
+++ src/elpv_dataset/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: 2024 Sergiu Deitsch <sergiu.deitsch@gmail.com>
+#
+# SPDX-License-Identifier: Apache-2.0
